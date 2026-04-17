370f66fc5bee63e696e4991b486d60c48e1440bc
tbsten <programmingcafeteria@gmail.com>
2026-04-17 17:16:53 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+rootProject.name = "Debuggable-Compiler-Plugin"
+
+pluginManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+include(":debuggable-runtime")
+
